33e68a733a196d3e7dbcb1c7d9781d39dfa56899
ashcherba <36545786+ashcherba@users.noreply.github.com>
2019-11-10 22:14:05 +0200

    HomeWork 7. Task 1
    1) Существуют три корабля. На каждом из них 10 ящиков груза. Они одновременно прибыли в порт в котором только два дока. Скорость разгрузки 1 ящик в 0.5 сек. Напишите программу которая управляя кораблями позволит им правильно разгрузить груз.

---
+++ Homework7/Ships.java
@@ -0,0 +1,53 @@
+package Homework7;
+
+import java.util.Arrays;
+
+public class Ships implements Runnable {
+	
+	private String name;
+	public int numberOfBoxes = 10;
+	public int[] docks = new int[2];
+	
+	public Ships() {
+		super();
+	}
+	
+	public Ships(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Ships [name=" + name + ", numberOfBoxes=" + numberOfBoxes + ", docks=" + Arrays.toString(docks) + "]";
+	}
+
+	@Override
+	public void run() {
+		
+		Thread thread = Thread.currentThread();
+		
+			int counter = 0;
+			for (int i = 0; i < numberOfBoxes; i++) {
+				System.out.println("Need to load " + (i + 1) + " boxes from " + name);
+				counter++;
+				try {
+					Thread.currentThread().sleep(500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			if (numberOfBoxes == counter) {
+				System.out.println(name + " is fully unloaded");
+			}
+		}
+
+}
